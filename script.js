64eb972ba4760e7ce5e57b28a2155e688f0fe171
fahhad2t-alt <fahhad2t@gmail.com>
2026-09-22 20:59:42 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,42 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBuGmGzpmIFPPX6E9RqZVRwVMD6s1LmJpw",
+  authDomain: "kol-hajah.firebaseapp.com",
+  projectId: "kol-hajah",
+  storageBucket: "kol-hajah.firebasestorage.app",
+  messagingSenderId: "965122607090",
+  appId: "1:965122607090:web:33c2c72354ca13d45fcd6b"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 const form = document.querySelector("form");
 
-form.addEventListener("submit", function(event) {
-    event.preventDefault();
+form.addEventListener("submit", async function(event) {
+  event.preventDefault();
 
-    const title = form.querySelector('input[type="text"]').value;
-    const price = form.querySelector('input[type="number"]').value;
-    const category = form.querySelector("select").value;
-    const description = form.querySelector("textarea").value;
-    const phone = form.querySelector('input[type="tel"]').value;
+  const inputs = form.querySelectorAll("input");
+  const select = form.querySelector("select");
+  const textarea = form.querySelector("textarea");
 
-    const ad = {
-        title: title,
-        price: price,
-        category: category,
-        description: description,
-        phone: phone
-    };
+  try {
+    await addDoc(collection(db, "requests"), {
+      title: inputs[0].value,
+      price: inputs[1].value,
+      category: select.value,
+      description: textarea.value,
+      phone: inputs[2].value,
+      createdAt: new Date()
+    });
 
-    localStorage.setItem("myAd", JSON.stringify(ad));
-
-    alert("تم نشر الإعلان بنجاح 🎉");
-
+    alert("تم نشر الإعلان بنجاح ✅");
     form.reset();
+
+  } catch (error) {
+    alert("حدث خطأ أثناء الحفظ");
+    console.error(error);
+  }
 });
